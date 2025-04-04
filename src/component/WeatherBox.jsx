f848ba58ad46130d81cdd9c7b6e3d5c374f3f5bb
yunho <Yunho7058@github.com>
2025-04-05 01:21:59 +0900

    2

--- src/component/WeatherBox.jsx
+++ src/component/WeatherBox.jsx
@@ -5,7 +5,7 @@ const WeatherBox = ({ weather }) => {
     <div>
       <div>{weather?.name}</div>
       <h2>
-        {weather?.main.temp}℃/{(weather?.main.temp * 1.8 + 32).toFixed(2)}℉
+        {weather?.main.temp}℃ / {(weather?.main.temp * 1.8 + 32).toFixed(2)}℉
       </h2>
       <h3>{weather?.weather[0].description}</h3>
     </div>
